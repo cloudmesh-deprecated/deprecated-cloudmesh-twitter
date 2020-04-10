3206610960ebcce417d77522265da2585189b3af
Gregor von Laszewski <laszewski@gmail.com>
2020-04-09 20:08:43 -0400

    wrong filename should be README.md

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@
 # limitations under the License.                                          #
 # ------------------------------------------------------------------------#
 
-from setuptools import find_packages, setup
 import io
+
+from setuptools import find_packages, setup
+
 
 def readfile(filename):
     with io.open(filename, encoding="utf-8") as stream:
@@ -35,7 +37,7 @@ tweepy
 
 version = readfile("VERSION")[0].strip()
 
-with open('README.md') as f:
+with open('README-old.md') as f:
     long_description = f.read()
 
 
